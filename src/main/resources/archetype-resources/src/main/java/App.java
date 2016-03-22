419bd351e31f2770243cc45e368879226795c2a2
Thanooj Kalathuru <javaeecub@gmail.com>
2016-03-22 16:01:13 +0530

    code changes

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,4 +1,4 @@
-package com.corejava;
+package ${package};
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -1,4 +1,4 @@
-package com.corejava;
+package ${package};
 
 import junit.framework.Test;
 import junit.framework.TestCase;
